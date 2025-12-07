4755de0b1a429dff9c5a291698259d93963c56ab
Mani Karthik <karthik07m@gmail.com>
2025-12-06 19:01:03 -0500

    base path changed

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/my_resum",
+  basePath: "/my_resume",
   images: {
     unoptimized: true,
   },
